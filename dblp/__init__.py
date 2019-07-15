2b0feeceab1adb875d335f79439a0ac5e632f222
omarjaved83 <https://OmarJaved83@bitbucket.org>
2019-07-15 10:18:35 +0200

    fixed search limit and output as json

--- dblp/__init__.py
+++ dblp/__init__.py
@@ -1,59 +1,45 @@
-from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 
 #options
 STRINGS_FOR_TEST = ["Collaborative Writing"]
 DBLP_BASE_URL = 'http://dblp.uni-trier.de/'
-PUB_SEARCH_URL = DBLP_BASE_URL + "search/publ/"
+PUB_SEARCH_URL = DBLP_BASE_URL + "search/publ/api"
 
 
 def query_db(pub_string=STRINGS_FOR_TEST):
     '''
-    returns the BeautifulSoup object of a query to DBLP
+    returns the Json object of a query to DBLP
 
     :param pub_string: A list of strings of keywords
-    :return: BeautifulSoup: A BeautifulSoup Object
+    :return: JSON: A JSON Object
     '''
-    resp = requests.get(PUB_SEARCH_URL, params={'q':pub_string})
-    return BeautifulSoup(resp.content)
+    resp = requests.get(PUB_SEARCH_URL, params={'q':pub_string, 'h': '1000', 'format' : 'json'})
 
-def get_pub_data(pub):
+    return resp.json()
+
+def get_pub_data(json):
     '''
     Extracts the information about a publication from a BeautifulSoup object
 
-    :param pub: A BeautifulSoup Object with Publication Information
+    :param pub: A JSON Object with Publication Information
     :return: dict: All Information of this Publication
     '''
-    ptype = 'nothing'
-    link = 'nothing'
-    authors = []
-    title = 'nothing'
-    where = 'nothing'
+    list_pub = []
+    publication_data = json['result']['hits']['hit']
+    for field in publication_data:
+        dict_info = {}
+        info = field['info']
+        if all (key in info for key in ("authors","url", "title", "type", "venue","year")):  # Some details are not available in dblp e.g., author or venue
+            dict_info['authors'] = info['authors']['author']
+            dict_info['url'] = info['url']
+            dict_info['title'] = info['title']
+            dict_info['type'] = info['type']
+            dict_info['where'] = info['venue']
+            dict_info['year'] = info['year']
+        list_pub.append(dict_info)
+    return list_pub
 
-    if 'year' in pub.get('class'):
-        # year is not always scrapable, except for this case. Might be done more elegantly
-        return int(pub.contents[0])
-    else:
-        ptype = pub.attrs.get('class')[1]
-        for content_item in pub.contents:
-            class_of_content_item = content_item.attrs.get('class', [0])
-            if 'data' in class_of_content_item:
-                for author in content_item.findAll('span', attrs={"itemprop": "author"}):
-                    authors.append(author.text)
-                title = content_item.find('span', attrs={"class": "title"}).text
-                for where_data in content_item.findAll('span', attrs={"itemprop": "isPartOf"}):
-                    found_where = where_data.find('span', attrs={"itemprop": "name"})
-                    if found_where:
-                        where = found_where.text
-            elif 'publ' in class_of_content_item:
-                link = content_item.contents[0].find('a').attrs.get('href', "nothing")
-
-    return {'Type': ptype,
-            'Link': link,
-            'Authors': authors,
-            'Title': title,
-            'Where': where}
 
 def search(search_string=STRINGS_FOR_TEST):
     '''
@@ -68,17 +54,9 @@ def search(search_string=STRINGS_FOR_TEST):
     :param search_string: A List of Strings of Keywords, that should be searched for
     :return: pd.DataFrame: A Dataframe with all data
     '''
-    soup = query_db(search_string)
-    pub_list_raw = soup.find("ul", attrs={"class": "publ-list"})
+    json = query_db(search_string)
+    pub  = get_pub_data(json)
+    return pd.DataFrame(pub)
 
-    pub_list_data = []
-    curr_year = 0
-    for child in pub_list_raw.children:
-        pub_data = get_pub_data(child)
-        if type(pub_data) == int:
-            curr_year = pub_data
-        else:
-            pub_data['Year'] = curr_year
-            pub_list_data.append(pub_data)
 
-    return pd.DataFrame(pub_list_data)
+
